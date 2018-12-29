6ad12575c043894d149dbefd137494bf47210e6c
Ronaldo Lima <ron@ldinho.com.br>
2018-12-29 14:36:19 -0200

    Improved Performance, React Lazy and Sespense

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import React, { Component } from 'react';
+import React, { Suspense } from 'react';
 import NavBarVivalinda from './ components/NavBarVivalinda' 
-import Footer from './ components/Footer'
-import './App.css';
 import logPhotos from './instagram'
+const AppPhoto = React.lazy( () => import( './AppPhoto' ))
+const Footer = React.lazy( () => import( './ components/Footer' ))
 
-class AppPhoto extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        value: []
-    };
-}
-componentDidMount() {
-    this.props.promise.then(value => {
-        this.setState({value});
-    });
-}
+const App = () => {
 
-  render() {
-    return (
-      <>
+  return(
+    <>
       <NavBarVivalinda/>
-      <div className="row no-gutters photoContainer">
-          {
-           this.state.value.map( ( item, i ) =>  { 
-             return (
-              <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={i} >
-                <img className="photoCard"   alt="" src={item} />
-             </div>
-           )
-           })
-         }
-      </div>
-      <Footer />
-      </>
-    )
-  }
+      <Suspense fallback={<div>Buscando Fotos...</div>} >
+        <AppPhoto promise={logPhotos()} />
+        </Suspense>
+      <Suspense fallback={<div>Contatos...</div>} >
+        <Footer />
+      </Suspense>
+    </>
+  )
 }
-
-const App = () => <div><AppPhoto promise={logPhotos()} /></div>
 
 export default App;
